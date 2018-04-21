2844783fddfbbda8b6245cba4610a79e69439740
Teagan Nouska <tnouska@live.com>
2018-04-21 15:02:23 -0500

    reducer added to reflections.reducer to set state to data from database

--- src/redux/reducers/reflections.reducer.js
+++ src/redux/reducers/reflections.reducer.js
@@ -1,4 +1,13 @@
 import { combineReducers } from "redux";
 
+//reflectionsList reducer
+export const reflectionsList = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_REFLECTIONS':
+            return action.payload
+        default:
+            return state;
+    };//end switch looking for 'SET_REFLECTIONS'
+};//end reflectionsList reducer
 
-// export default combineReducers({  }) will add reducers later
+export default combineReducers({ reflectionsList }) 
